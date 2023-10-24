92c968edd3ae898711f81af730e513dd3d8c29c9
Pkraygithub <pankaj9576.kr@gmail.com>
2023-10-24 22:17:51 +0530

    First Commit

---
+++ TestJava/src/TestPack/Pelindum.java
@@ -0,0 +1,27 @@
+package TestPack;
+
+public class Pelindum {
+
+	public static void main(String[] args) {
+
+
+		String s1="DAD";
+		
+		String s2="";
+		
+		for(int i=s1.length()-1;i>=0;i--) {
+			
+			s2+=s1.charAt(i);
+		}
+		
+		System.out.println(s2);
+		
+		if(s1.equals(s2)) {
+			
+			System.out.println("This is Pelandrum");
+		}else {
+			System.out.println("This is not Pelandrum");
+		}
+	}
+
+}
